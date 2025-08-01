bc2704995f658f25da8a82cd9dea71b6932e3866
Hedley Luna <hluna@stackstate.com>
2025-08-01 15:20:23 +0200

    enablinng all stores and items

--- src/config/stores.ts
+++ src/config/stores.ts
@@ -14,31 +14,31 @@ export const STORES: Store[] = [
     searchParam: 'q',
     requiresBrowser: true
   },
-  // {
-  //   name: 'Coolblue',
-  //   baseUrl: 'https://www.coolblue.nl',
-  //   searchPath: '/zoeken',
-  //   searchParam: 'query',
-  //   requiresBrowser: false
-  // },
-  // {
-  //   name: 'Alternate',
-  //   baseUrl: 'https://www.alternate.nl',
-  //   searchPath: '/listing.xhtml',
-  //   searchParam: 'q',
-  //   requiresBrowser: false
-  // },
-  // {
-  //   name: "Azerty",
-  //   baseUrl: "https://azerty.nl",
-  //   searchPath: "/catalogsearch/result/",
-  //   searchParam: "q",
-  //   requiresBrowser: false,
-  // },
+  {
+    name: 'Coolblue',
+    baseUrl: 'https://www.coolblue.nl',
+    searchPath: '/zoeken',
+    searchParam: 'query',
+    requiresBrowser: false
+  },
+  {
+    name: 'Alternate',
+    baseUrl: 'https://www.alternate.nl',
+    searchPath: '/listing.xhtml',
+    searchParam: 'q',
+    requiresBrowser: false
+  },
+  {
+    name: "Azerty",
+    baseUrl: "https://azerty.nl",
+    searchPath: "/catalogsearch/result/",
+    searchParam: "q",
+    requiresBrowser: false,
+  },
 ];
 
 export const SEARCH_TERMS = [
-  "Radeon 7700XT", // 'Radeon 7900XT', 'Radeon 7900XTX',
-  //'GeForce 5070 ti', 'GeForce 5080', 'GeForce 4070 TI Super',
-  // 'Ryzen 9950X3D', 'Ryzen 9900X3D', 'Ryzen 7950X3D', 'Ryzen 7800X3D', 'Ryzen 7900X3D'
+  "Radeon 7700XT", 'Radeon 7900XT', 'Radeon 7900XTX',
+  'GeForce 5070 ti', 'GeForce 5080', 'GeForce 4070 TI Super',
+  'Ryzen 9950X3D', 'Ryzen 9900X3D', 'Ryzen 7950X3D', 'Ryzen 7800X3D', 'Ryzen 7900X3D'
 ];
