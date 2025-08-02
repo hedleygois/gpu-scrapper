bcc2123a9c7407482b3ccbd764ddd1e27be4f8a9
Hedley Luna <hluna@stackstate.com>
2025-08-02 23:35:31 +0200

    better names to avoid finding unrelated stuff

--- src/config/stores.ts
+++ src/config/stores.ts
@@ -8,37 +8,37 @@ export interface Store {
 
 export const STORES: readonly Store[] = [
   {
-    name: 'Megekko',
-    baseUrl: 'https://www.megekko.nl',
-    searchPath: '/zoeken',
-    searchParam: 'q',
+    name: "Megekko",
+    baseUrl: "https://www.megekko.nl",
+    searchPath: "/zoeken",
+    searchParam: "q",
     requiresBrowser: true
   },
-  // {
-  //   name: 'Coolblue',
-  //   baseUrl: 'https://www.coolblue.nl',
-  //   searchPath: '/zoeken',
-  //   searchParam: 'query',
-  //   requiresBrowser: false
-  // },
-  // {
-  //   name: 'Alternate',
-  //   baseUrl: 'https://www.alternate.nl',
-  //   searchPath: '/listing.xhtml',
-  //   searchParam: 'q',
-  //   requiresBrowser: false
-  // },
-  // {
-  //   name: "Azerty",
-  //   baseUrl: "https://azerty.nl",
-  //   searchPath: "/catalogsearch/result/",
-  //   searchParam: "q",
-  //   requiresBrowser: false,
-  // },
+  {
+    name: "Coolblue",
+    baseUrl: "https://www.coolblue.nl",
+    searchPath: "/zoeken",
+    searchParam: "query",
+    requiresBrowser: false
+  },
+  {
+    name: "Alternate",
+    baseUrl: "https://www.alternate.nl",
+    searchPath: "/listing.xhtml",
+    searchParam: "q",
+    requiresBrowser: false
+  },
+  {
+    name: "Azerty",
+    baseUrl: "https://azerty.nl",
+    searchPath: "/catalogsearch/result/",
+    searchParam: "q",
+    requiresBrowser: false,
+  },
 ] as const;
 
 export const SEARCH_TERMS: readonly string[] = [
-  "Radeon 7700XT", // 'Radeon 7900XT', 'Radeon 7900XTX',
-  // 'GeForce 5070 ti', 'GeForce 5080', 'GeForce 4070 TI Super',
-  // 'Ryzen 9950X3D', 'Ryzen 9900X3D', 'Ryzen 7950X3D', 'Ryzen 7800X3D', 'Ryzen 7900X3D'
+  "AMD Radeon 7700XT", "AMD Radeon 7900XT", "AMD Radeon 7900XTX",
+  "NVIDIA GeForce 5070 ti", "NVIDIA GeForce 5080", "NVIDIA GeForce 4070 TI Super",
+  "AMD Ryzen 9950X3D", "AMD Ryzen 9900X3D", "AMD Ryzen 7950X3D", "AMD Ryzen 7800X3D", "AMD Ryzen 7900X3D"
 ] as const;
